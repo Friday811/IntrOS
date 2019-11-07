0dd82addf952114289c112aa1ed018d41e97ed13
Rafael Ortiz <rafael@ortiz.sh>
2019-11-07 14:03:06 -0500

    Test our printf()

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -1,160 +1,22 @@
 #include <stdio.h>
 #include <kernel/tty.h>
+#include <limits.h>
 
 void kernel_main(void) 
 {
     terminal_initialize();
     printf("Hello, kernel World!\n");
+    for (size_t i = 0; i < 12; i++)
+    {
+        printf("Num: %i\n", i);
+    }
+    printf("printf() with char: '%c', int: %i, and string: \"%s\"\n",
+        'c', 15, "hello!");
+    
+    printf("This is a very long line that should go well past 80 chars, %s\n",
+        "in fact it should probably wrap around to the next line.");
+    printf("We can also scroll...\n\n\n\n\n\n\n\n\n\n\n\n\n");
+
+    printf("Display big numbers: %i\n", INT_MAX);
+    printf("...and small ones: %i\n", INT_MIN);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
